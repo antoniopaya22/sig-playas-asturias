3868b4edb8779484138b2302f0738e3a1bd8b117
Antonio Paya <antonioalfa22@gmail.com>
2020-09-17 12:09:38 +0200

    Añadido menu al frontend

---
+++ sig-frontend/pages/about.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Link from '../components/Link';
+import Footer from '../components/Footer';
+
+export default function About() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom> Next.js example </Typography>
+        <Button variant="contained" color="primary" component={Link} naked href="/">
+          Go to the main page
+        </Button>
+      </Box>
+    </Container>
+  );
+}
