028a0ce7d4f53a3ef712a6a9eb9e0cbe91839060
Antonio Paya <antonioalfa22@gmail.com>
2020-09-21 19:08:43 +0200

    Añadidos los modelos de Beach y Occupation.

--- sig-backend/api/repository/beach_repository.py
+++ sig-backend/api/repository/beach_repository.py
@@ -11,4 +11,34 @@ class BeachRepository:
         except:
             return []
 
+    @staticmethod
+    def add_beach(beach):
+        try:
+            db.session.add(beach)
+            db.session.commit()
+        except:
+            return False
+        return True
 
+    @staticmethod
+    def delete_beach(id):
+        try:
+            Beach.query.filter_by(id=id).delete()
+            db.session.commit()
+        except:
+            return False
+        return True
+
+    @staticmethod
+    def get_beach_by_playa_id(playa_id):
+        try:
+            return Beach.query.filter_by(playa_id=playa_id).one()
+        except:
+            return None
+
+    @staticmethod
+    def get_beach_by_id(id):
+        try:
+            return Beach.query.get(id)
+        except:
+            return None
